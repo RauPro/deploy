acc4eefb041d34183ad5716560a6917b0c2808c4
RauPro <00012119@uca.edu.sv>
2021-03-24 22:26:34 -0600

    Heorku

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { IDataProducts } from '../interfaces/idata-products';
 
-@Injectable()
+
+@Injectable({
+  providedIn: 'root',
+})
 export class StorageService {
   constructor() {}
   saveToken(token: string): void {
